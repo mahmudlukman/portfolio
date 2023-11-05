918e4a0958d02fb68d0f3acf2f0703444ceabd79
Mahmud Lukman <mistamann49@gmail.com>
2023-11-05 19:54:42 +0100

    Home Page Complete

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,68 @@
+// next image
+import Image from 'next/image';
+
+// components
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
+
+// framer motion
+import { motion } from 'framer-motion';
+
+// variants
+import { fadeIn } from '../variants';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* title */}
+          <motion.h1 
+            className="h1"
+            variants={fadeIn('down', 0.2)} 
+            initial='hidden' 
+            animate='show' 
+            exit='hidden'
+          >
+            Transforming Ideas <br /> Into{' '}
+            <span className="text-accent">Digital Reality</span>
+          </motion.h1>
+          {/* subtitle */}
+          <motion.p 
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+            variants={fadeIn('down', 0.3)} 
+            initial='hidden' 
+            animate='show' 
+            exit='hidden'
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </motion.p>
+          {/* btn */}
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+          <motion.div 
+            variants={fadeIn('down', 0.4)} 
+            initial='hidden' 
+            animate='show' 
+            exit='hidden'
+            className='hidden xl:flex'
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      {/* image */}
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;
