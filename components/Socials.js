5415bac90f2dc5f565aaa3f5aea207a035b07684
Mahmud Lukman <mistamann49@gmail.com>
2023-11-06 13:31:57 +0100

    Home page complete

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,26 @@
+// links
+import Link from "next/link";
+
+// icons
+import {RiGithubLine, RiInstagramLine, RiFacebookLine, RiTwitterLine} from 'react-icons/ri'
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiTwitterLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
