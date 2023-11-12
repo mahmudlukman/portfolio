8a8fbc169a1e94e067c0227f93cd927b7656a68c
Mahmud Lukman <mistamann49@gmail.com>
2023-11-12 15:07:34 +0100

    netlify cleanup

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,13 @@ import { fadeIn } from '../variants';
 const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
+      {/* A little help for the Netlify post-processing bots  */}
+      <form name="contact" netlify netlify-honeypot="bot-field" action='/' hidden>
+        <input type="text" name="name" />
+        <input type="text" name="subject" />
+        <input type="email" name="email" />
+        <textarea name="message"></textarea>
+      </form>
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
